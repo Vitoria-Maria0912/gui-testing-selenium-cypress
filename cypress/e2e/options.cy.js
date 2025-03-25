3fbb554b5a83a6bdb76849ca976cc6892a091c46
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2025-03-24 23:56:03 -0300

    test: Create test when option name (in English) its forgotten.

--- cypress/e2e/options.cy.js
+++ cypress/e2e/options.cy.js
@@ -1,12 +1,14 @@
 describe('options', () => {
+
   beforeEach(() => {
     cy.visit('/admin');
     cy.get('[id="_username"]').type('sylius');
     cy.get('[id="_password"]').type('sylius');
     cy.get('.primary').click();
+    cy.clickInFirst('a[href="/admin/product-options/"]');
   });
-  // Remove .only and implement others test cases!
-  it.only('edit size XL to GG in Portuguese (Portugal)', () => {
+
+  it('edit size XL to GG in Portuguese (Portugal)', () => {
     // Click in options in side menu
     cy.clickInFirst('a[href="/admin/product-options/"]');
     // Type in value input to search for specify option
@@ -23,12 +25,23 @@ describe('options', () => {
     // Assert that option has been updated
     cy.get('body').should('contain', 'Product option has been successfully updated.');
   });
-  it('test case 2', () => {
-    // Implement your test case 2 code here
-  });
-  it('test case 3', () => {
-    // Implement your test case 3 code here
+
+  it('create a new product, forgetting to add a product name in English', () => {
+    cy.get('*[class^="ui right floated buttons"]').click();
+    cy.get('#sylius_product_option_code').type("13747397");
+    cy.get('#sylius_product_option_position').type(999);
+    cy.get('*[class^="ui styled fluid accordion"]');
+    cy.contains('.title', 'Spanish (Mexico)').click();
+    cy.get('#sylius_product_option_translations_es_MX_name').type("Teste");
+    cy.contains('*[class^="ui labeled icon button"]', 'Add value').click();
+    cy.get('#sylius_product_option_values_0_code').type("7485899");
+    cy.get('#sylius_product_option_values_0_translations_en_US_value').type("Teste");
+    cy.contains('*[class^="ui labeled icon primary button"]', 'Create').click();
+    
+    // Assert that product has been created
+    cy.get('body').should('contain', 'This form contains errors.')
   });
 
-  // Implement the remaining test cases in a similar manner
+  // it('test case 3', () => {
+  // });
 });
